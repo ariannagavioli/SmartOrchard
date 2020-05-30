9ec4fe578c6c958d4eab996e134bfee1060564f3
ariannagavioli <gavioli.arianna@gmail.com>
2020-05-30 22:51:51 +0200

    Soil Temperature resource

--- LLNDevices/Resources/res_soil_temp.c
+++ LLNDevices/Resources/res_soil_temp.c
@@ -3,12 +3,12 @@
 #include "contiki.h"
 #include "coap-engine.h"
 
-static double temperature = 25;
+static double temperature	= 25;
+static int increasing_sign	= 1;		//Either 1 or -1 depending on the period of the day (can be modified through a put on the resource)
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
 
-/* A simple actuator example, depending on the color query parameter and post variable mode, corresponding led is activated or deactivated */
 EVENT_RESOURCE(res_soil_temp,
          "title=\"Soil Temperature\";obs",
          res_get_handler,
@@ -18,13 +18,9 @@ EVENT_RESOURCE(res_soil_temp,
          res_event_handler);
          
 static void res_event_handler(void) {
-	static double random_value = rand() / RAND_MAX;				//returns a random value between 0 and 1
-	static double random_sign = rand()/RAND_MAX - 0.5;			//returns a random value between -0.5 and 0.5
-	if(random_sign > 0) {
-		temperature += random_value;
-	} else {
-		temperature -= random_value;
-	}
+	static double random_value = rand() / RAND_MAX;			//returns a random value between 0 and 1
+	
+	temperature += (increasing_sign * random_value);			//temperature can change up to 1°C
 	
 	if(temperature > 40)
 		temperature = 40;
@@ -44,5 +40,8 @@ static void res_get_handler(coap_message_t *request, coap_message_t *response, u
 	}
 	
 	coap_set_header_content_format(response, TEXT_PLAIN);
-	coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil temperature\":%f}", req_temperature));
+	if()
+		coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil temperature\":%f °C}", req_temperature));
+	else
+		coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil temperature\":%f °F}", req_temperature));
 }
