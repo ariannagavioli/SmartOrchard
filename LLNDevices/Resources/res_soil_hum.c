3bf6868249789b166e92e21d195873ce5361c3c8
ariannagavioli <gavioli.arianna@gmail.com>
2020-05-30 22:44:54 +0200

    Soil Humidity resource

--- LLNDevices/Resources/res_soil_hum.c
+++ LLNDevices/Resources/res_soil_hum.c
@@ -3,12 +3,12 @@
 #include "contiki.h"
 #include "coap-engine.h"
 
-static double humidity = 85;		//humidity percentage
+static double humidity		= 85;		//humidity percentage
+static int increasing_sign	= 1;		//Either 1 or -1 depending on the period of the day (can be modified through a put on the resource)
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void res_event_handler(void);
 
-/* A simple actuator example, depending on the color query parameter and post variable mode, corresponding led is activated or deactivated */
 EVENT_RESOURCE(res_soil_hum,
          "title=\"Soil Humidity\";obs",
          res_get_handler,
@@ -19,13 +19,10 @@ EVENT_RESOURCE(res_soil_hum,
          
 static void res_event_handler(void) {
 	static double random_value = 3 * rand() / RAND_MAX;				//returns a random value between 0 and 3
-	static double random_sign = rand()/RAND_MAX - 0.5;				//returns a random value between -0.5 and 0.5
-	if(random_sign > 0) {
-		humidity += random_value;
-	} else {
-		humidity -= random_value;
-	}
 	
+	humidity += (increasing_sign * random_value);					//humidity can change up to 3%
+	
+	/* Keeping humidity as a percentage measure */
 	if(humidity > 100)
 		humidity = 100;
 	else if(humidity < 0)
@@ -36,6 +33,6 @@ static void res_event_handler(void) {
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {	
 	coap_set_header_content_format(response, TEXT_PLAIN);
-	coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil humidity\":%f}", humidity));
+	coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil humidity\":%f %}", humidity));
 }
 
