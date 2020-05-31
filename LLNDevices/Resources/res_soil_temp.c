a495ca9ecac0c91838abedb5a31a21cb5290bb93
ariannagavioli <gavioli.arianna@gmail.com>
2020-05-31 11:36:42 -0400

    Soil Temperature resource

--- LLNDevices/Resources/res_soil_temp.c
+++ LLNDevices/Resources/res_soil_temp.c
@@ -18,7 +18,8 @@ EVENT_RESOURCE(res_soil_temp,
          res_event_handler);
          
 static void res_event_handler(void) {
-	static double random_value = rand() / RAND_MAX;			//returns a random value between 0 and 1
+	static double random_value;
+	random_value = rand() / RAND_MAX;			//returns a random value between 0 and 1
 	
 	temperature += (increasing_sign * random_value);			//temperature can change up to 1°C
 	
@@ -32,15 +33,23 @@ static void res_event_handler(void) {
 
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {	
 	
-	double req_temperature = temperature;			// Default in °C
+	const char *degree = NULL;
+	static double req_temperature;
+	req_temperature = temperature;			// Default in °C
+	static int isCelsius = 1;
 	
 	//Is conversion needed?
-	if() {
-		req_temperature = req_temperature * 9 / 5 + 32;
+	if(coap_get_query_variable(request, "degree", &degree)) {
+		static char degree_val[1];
+		memcpy(degree_val, degree, 1);
+		if(!strcmp("F",degree_val))	{					// If client requested it to be in °F
+			req_temperature = req_temperature * 9 / 5 + 32;
+			isCelsius = 0;
+		}
 	}
 	
 	coap_set_header_content_format(response, TEXT_PLAIN);
-	if()
+	if(isCelsius)
 		coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil temperature\":%f °C}", req_temperature));
 	else
 		coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil temperature\":%f °F}", req_temperature));
