4ee1dae3ded61231ad0887ca3aa8eadbdfa345e9
Arianna Gavioli <gavioli.arianna@gmail.com>
2020-06-01 17:43:34 +0200

    Fruit Ripeness Monitoring server

--- LLNDevices/CoAP Servers/ripeness_monitoring_server.c
+++ LLNDevices/CoAP Servers/ripeness_monitoring_server.c
@@ -14,17 +14,17 @@
 PROCESS(ripeness_camera_server, "Ripeness Camera CoAP Server");
 AUTOSTART_PROCESSES(&ripeness_camera_server);
 
-extern coap_resource_t res_ripeness_camera;
+extern coap_resource_t res_ripeness_cam;
 
 static int observing_interval = 60;
 static struct etimer et;
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(soil_monitor_server, ev, data){
+PROCESS_THREAD(ripeness_camera_server, ev, data){
 	PROCESS_BEGIN();
 
 	LOG_INFO("Starting Ripeness Camera Server\n");
 
-	coap_activate_resource(&res_ripeness_camera, "soil_ripeness_camera");
+	coap_activate_resource(&res_ripeness_cam, "soil_ripeness_cam");
 
 	etimer_set(&et, CLOCK_SECOND * observing_interval);
 
@@ -33,7 +33,7 @@ PROCESS_THREAD(soil_monitor_server, ev, data){
 		PROCESS_WAIT_EVENT();
 
 		if(ev == PROCESS_EVENT_TIMER && data == &et){
-			res_ripeness_camera.trigger();
+			res_ripeness_cam.trigger();
 			etimer_set(&et, CLOCK_SECOND * observing_interval);
 		}
 
