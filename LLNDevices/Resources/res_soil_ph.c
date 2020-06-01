e0d5cbf0338794946b93b9485717161646d28ef8
Arianna Gavioli <gavioli.arianna@gmail.com>
2020-06-01 17:46:16 +0200

    Soil pH resource

--- LLNDevices/Resources/res_soil_ph.c
+++ LLNDevices/Resources/res_soil_ph.c
@@ -18,9 +18,10 @@ EVENT_RESOURCE(res_soil_ph,
          
 static void res_event_handler(void) {
 	static double random_value;
-	random_value = 0.2 * rand() / RAND_MAX;				//returns a random value between 0 and 0.2
+	random_value = rand() % 3;				
+	random_value *= 0.1;				//returns a random value between 0 and 0.2
 	static double increasing_sign;
-	increasing_sign = rand() / RAND_MAX - RAND_MAX / 2;	//returns a random value between -0.5 and 0.5
+	increasing_sign = rand() % 2;	//returns either 0 or 1
 
 	if(increasing_sign > 0)
 		soil_ph += random_value;									//soil_ph can increase of up to 0.2
@@ -36,9 +37,36 @@ static void res_event_handler(void) {
 	coap_notify_observers(&res_soil_ph);
 }
 
+
+/* For simulation purposes, a post call will adjust the pH resource, increasing it or decreasing it*/
 static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset) {	
-	coap_set_header_content_format(response, TEXT_PLAIN);
-	coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "{\"Soil pH\":%f}", soil_ph));
+	const char *increase = NULL;
+	const char *decrease = NULL;
+
+	static int length1;
+	static int length2;
+	
+	length1 = coap_get_post_variable(request, "increase", &increase);
+	length2 = coap_get_post_variable(request, "decrease", &decrease);
+
+	if(length1) {
+
+		static int amount = atoi(increase);
+		humidity += amount;
+
+		coap_set_status_code(response, CHANGED_2_04);
+
+	} else if(length2) {
+
+		static int amount = atoi(decrease);
+		humidity -= amount;
+
+		coap_set_status_code(response, CHANGED_2_04);
+
+	} else {
+		coap_set_status_code(response, BAD_REQUEST_4_00);
+	}
+
 }
 
 
