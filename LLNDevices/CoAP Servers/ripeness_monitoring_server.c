82727de2b94d24c5160abcbe69044f719945f208
ariannagavioli <gavioli.arianna@gmail.com>
2020-06-02 17:21:46 -0400

    Ripeness Camera server

--- LLNDevices/CoAP Servers/ripeness_monitoring_server.c
+++ LLNDevices/CoAP Servers/ripeness_monitoring_server.c
@@ -23,8 +23,22 @@ PROCESS_THREAD(ripeness_camera_server, ev, data){
 	PROCESS_BEGIN();
 
 	LOG_INFO("Starting Ripeness Camera Server\n");
-
-	coap_activate_resource(&res_ripeness_cam, "soil_ripeness_cam");
+	
+	/* REGISTRATION TO THE CLOUD */
+	static coap_endpoint_t server_ep;
+	static coap_message_t request[1];
+	coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+	coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+	coap_set_header_uri_path(request, service_url);
+	// Set the registration information
+	static char msg[128];
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "{\"Functionality\":\"ripeness_camera\",\"Node Number\":%d}",node_id);
+	coap_set_payload(request, (uint8_t *)msg, strlen(msg));
+	COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+	
+	/* ACTIVATION OF THE SERVER */
+	coap_activate_resource(&res_ripeness_cam, "ripeness_cam");
 
 	etimer_set(&et, CLOCK_SECOND * observing_interval);
 
