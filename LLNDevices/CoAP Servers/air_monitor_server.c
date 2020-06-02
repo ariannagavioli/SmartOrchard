f2264bf65e249304ce69dd36681838501e422a75
ariannagavioli <gavioli.arianna@gmail.com>
2020-06-02 15:45:09 -0400

    Air Monitor server

--- LLNDevices/CoAP Servers/air_monitor_server.c
+++ LLNDevices/CoAP Servers/air_monitor_server.c
@@ -5,11 +5,29 @@
 #include "contiki.h"
 #include "coap-engine.h"
 #include "sys/etimer.h"
+#include "node-id.h"
 
 /* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_APP
+
+#define DEBUG 1
+
+// Registration Server IP and resource path
+#define SERVER_EP "coap://127.0.0.1/5683"
+char *service_url = "/registration";
+
+// Define a handler to handle the response from the server
+void client_chunk_handler(coap_message_t *response) {
+	const uint8_t *chunk;
+	if(response == NULL) {
+		puts("Request timed out");
+		return;
+	}
+	int len = coap_get_payload(response, &chunk);
+	printf("|%.*s", len, (char *)chunk);
+}
 
 PROCESS(air_monitor_server, "Air Monitor CoAP Server");
 AUTOSTART_PROCESSES(&air_monitor_server);
@@ -17,14 +35,28 @@ AUTOSTART_PROCESSES(&air_monitor_server);
 extern coap_resource_t res_air_temp;
 extern coap_resource_t res_air_hum;
 
-static int observing_interval = 20;
+static int observing_interval = 2;
 static struct etimer et;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(air_monitor_server, ev, data){
 	PROCESS_BEGIN();
 
 	LOG_INFO("Starting Air Monitoring Server\n");
-
+	
+	/* REGISTRATION TO THE CLOUD */
+	static coap_endpoint_t server_ep;
+	static coap_message_t request[1];
+	coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+	coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+	coap_set_header_uri_path(request, service_url);
+	// Set the registration information
+	static char msg[128];
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "{\"Functionality\":\"air_monitor\",\"Node Number\":%d}",node_id);
+	coap_set_payload(request, (uint8_t *)msg, strlen(msg));
+	COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+	
+	/* ACTIVATION OF THE SERVER */
 	coap_activate_resource(&res_air_temp, "air_temp");
 	coap_activate_resource(&res_air_hum, "air_hum");
 
