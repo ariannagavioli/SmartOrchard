cdb4a7332af6c7e1c850d0992f8fae8689060af1
ariannagavioli <gavioli.arianna@gmail.com>
2020-06-12 12:49:41 +0200

    Soil Monitoring server

--- LLNDevices/CoAP Servers/soil_monitoring_server.c
+++ LLNDevices/CoAP Servers/soil_monitoring_server.c
@@ -4,22 +4,43 @@
 #include <unistd.h>
 #include "contiki.h"
 #include "coap-engine.h"
+#include "coap-blocking-api.h"
 #include "sys/etimer.h"
+#include "node-id.h"
+#include "net/routing/routing.h"
+#include "net/netstack.h"
+#include "net/ipv6/uip.h"
+#include "net/ipv6/simple-udp.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ipv6/uiplib.h"
 
 /* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_APP
 
-PROCESS(air_monitor_server, "Soil Monitor CoAP Server");
-AUTOSTART_PROCESSES(&air_monitor_server);
+// Registration Server IP and resource path
+#define SERVER_EP "coap://[fd00::1]:5683"
+char *service_url = "/registration";
 
-extern coap_resource_t res_soil_temp;
-extern coap_resource_t res_soil_hum;
-extern coap_resource_t res_soil_ph;
-extern coap_resource_t res_soil_sal;
+// Define a handler to handle the response from the server
+void client_chunk_handler(coap_message_t *response) {
+	const uint8_t *chunk;
+	if(response == NULL) {
+		puts("Request timed out");
+		return;
+	}
+	int len = coap_get_payload(response, &chunk);
+	printf("Successfully registered.\n", len, (char *)chunk);
+}
 
-static int observing_interval = 30;
+PROCESS(soil_monitor_server, "Soil Monitor CoAP Server");
+AUTOSTART_PROCESSES(&soil_monitor_server);
+
+extern coap_resource_t res_soil;
+
+static int observing_interval = 6;
+static int configuration_interval = 0.5;
 static struct etimer et;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(soil_monitor_server, ev, data){
@@ -27,24 +48,42 @@ PROCESS_THREAD(soil_monitor_server, ev, data){
 
 	LOG_INFO("Starting Soil Monitoring Server\n");
 	
-	/* REGISTRATION TO THE CLOUD */
+	coap_activate_resource(&res_soil, "soil");
+	
+	/* REGISTRATION PHASE */
+	//Retrieve global ip
+	etimer_set(&et, CLOCK_SECOND * configuration_interval);
+	while(!NETSTACK_ROUTING.node_is_reachable()){
+		PROCESS_WAIT_EVENT();
+		if(ev == PROCESS_EVENT_TIMER && data == &et){
+			etimer_set(&et, CLOCK_SECOND * configuration_interval);
+		}
+	}
+	static char ip_address[128];
+	memset(ip_address, 0, sizeof(ip_address));
+	static int i;
+	static int found = 0;
+	uint8_t state;
+	for(i = 0; i < UIP_DS6_ADDR_NB && !found; i++) {
+		state = uip_ds6_if.addr_list[i].state;
+		if(uip_ds6_if.addr_list[i].isused && (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+			found++;
+			uiplib_ipaddr_snprint(ip_address, sizeof(ip_address), &(uip_ds6_if.addr_list[i].ipaddr));
+		}
+	}
+	// Set the registration information
+	static char msg[128];
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "{\"Functionality\":\"soil_monitor\",\"ID\":%d,\"IP\":\"%s\"}",node_id,ip_address);
+	
+	/* Registration to the cloud */
 	static coap_endpoint_t server_ep;
 	static coap_message_t request[1];
 	coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
 	coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
 	coap_set_header_uri_path(request, service_url);
-	// Set the registration information
-	static char msg[128];
-	memset(msg, 0, sizeof(msg));
-	sprintf(msg, "{\"Functionality\":\"soil_monitor\",\"Node Number\":%d}",node_id);
-	coap_set_payload(request, (uint8_t *)msg, strlen(msg));
+	coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
 	COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
-	
-	/* ACTIVATION OF THE SERVER */
-	coap_activate_resource(&res_soil_temp, "soil_temp");
-	coap_activate_resource(&res_soil_hum, "soil_hum");
-	coap_activate_resource(&res_soil_ph, "soil_ph");
-	coap_activate_resource(&res_soil_sal, "soil_sal");
 
 
 	etimer_set(&et, CLOCK_SECOND * observing_interval);
@@ -54,10 +93,7 @@ PROCESS_THREAD(soil_monitor_server, ev, data){
 		PROCESS_WAIT_EVENT();
 
 		if(ev == PROCESS_EVENT_TIMER && data == &et){
-			res_soil_hum.trigger();
-			res_soil_temp.trigger();
-			res_soil_ph.trigger();
-			res_soil_sal.trigger();
+			res_soil.trigger();
 			etimer_set(&et, CLOCK_SECOND * observing_interval);
 		}
 
